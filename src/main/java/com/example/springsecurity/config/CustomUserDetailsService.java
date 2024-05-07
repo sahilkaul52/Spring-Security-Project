a89720ed330469cfa211e181de970f2916a8e758
sahilkaul52 <sahilkaul52@gmail.com>
2024-05-07 21:23:18 +0530

    Implementing Custom UserDetailsManager, CustomAuthenticationProvider and Authorization

---
+++ src/main/java/com/example/springsecurity/config/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+/*
+package com.example.springsecurity.config;
+
+import com.example.springsecurity.model.Customer;
+import com.example.springsecurity.repository.CustomerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service TODO This service will be called by DAOAuthenticationProvider but since we are using our own custom AuthenticationProvoder, we don't need this service rn
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String userName, password;
+        List<GrantedAuthority> authorities;
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if (customer.size() == 0) {
+            throw new UsernameNotFoundException("User details not found for the user : " + username);
+        } else{
+            userName = customer.get(0).getEmail();
+            password = customer.get(0).getPwd();
+            authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+        }
+        return new User(userName,password,authorities);
+    }
+
+}*/
